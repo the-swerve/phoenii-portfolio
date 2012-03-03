005bbc2830be3a8737db3260e3b6bbb5a33ddf44
aesacus <jayrbolton@gmail.com>
2012-03-02 20:39:03 -0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Phoenii::Application.config.secret_token = '259659a5822588b0dac31a406e0778e4ed31dfddddb6d06ff0df0b45282653369da468c140617062d7d6489b093a5bc83d66776205ad1c62eebe562e36ed472c'
